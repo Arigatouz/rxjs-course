25e2b9d785b96c190aa0b0ecc8a26fb21fdd10a6
Ali Gamal <aga@luftborn.com>
2022-08-17 20:55:21 +0200

    testing observables

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Observable, fromEvent, timer } from "rxjs";
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  private _click: Observable<Event>;
+  constructor() {}
 
   ngOnInit() {
+    this._click = fromEvent(document, "click");
+    this._click.subscribe((event) => {
+      console.log(event);
+    });
   }
-
 }
